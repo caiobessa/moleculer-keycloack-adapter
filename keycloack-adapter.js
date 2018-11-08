b563941d2edcf8a7738707004f47bd15bce76450
bessa@hotmail.com.br <bessa@hotmail.com.br>
2018-11-07 22:33:57 -0200

    getting better the API

--- keycloack-adapter.js
+++ keycloack-adapter.js
@@ -1,19 +1,15 @@
 const KeycloakConnect = require('keycloak-connect');
 
-const config = {
-  'auth-server-url': process.env.KEYCLOAK_SERVER_URL,
-  resource: process.env.KEYCLOAK_RESOURCE_NAME,
-  realm: process.env.KEYCLOAK_REALM || 'mctic',
-  bearerOnly: process.env.KEYCLOAK_BEARER_ONLY || true,
-  'public-client': process.env.KEYCLOAK_PUBLIC_CLIENT || true,
-  'ssl-required': process.env.KEYCLOAK_SSL_REQUIRED || 'external'
-};
 
-let keycloak = new KeycloakConnect({}, config);
 
-keycloak.accessDenied = (request, response) => {
-  response.writeHead(403);
-  response.end('Accesss Denied');
-};
+const createKeycloakConnect = config  =>{
+  let keycloak = new KeycloakConnect({}, config);
+  keycloak.accessDenied = (request, response) => {
+    response.writeHead(403);
+    response.end('Accesss Denied');
+  }; 
+  return [...keycloak.middleware(), keycloak.protect()];
+}
 
-module.exports = [...keycloak.middleware(), keycloak.protect()];
+
+module.exports = createKeycloakConnect;
